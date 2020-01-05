87eb7863971e555eeaa8888b6b51324ca091c19d
Serguei Rodriguez <sjosevich@gmail.com>
2020-01-04 17:54:20 -0700

    Created the function readProducts in order to show or display all the available products

---
+++ bamazonCustomer.js
@@ -0,0 +1,32 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "bamazon_db"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    readProducts();
+  });
+
+  function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      connection.end();
+    });
+  }
